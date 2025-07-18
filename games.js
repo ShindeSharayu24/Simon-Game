b15541a7194495026a2338841dfd721f89f34755
Sharayu Shinde <120458049+ShindeSharayu24@users.noreply.github.com>
2025-07-18 16:25:06 +0530

    Update games.js

--- games.js
+++ games.js
@@ -61,7 +61,7 @@ function nextSequence() {
 }
 
 function playSound(name) {
-    let audio = new Audio("sounds/" + name + ".mp3");
+    let audio = new Audio(name + ".mp3");
     audio.play();
  }
 
